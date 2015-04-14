2e0fe84f51a7f2a92257e020b83ad589e37e97da
Aggelos Avgerinos <evaggelos.avgerinos@gmail.com>
2015-04-14 21:36:06 +0300

    [Fix #1622] Remove mandatory requiring of Fog gem

--- lib/carrierwave/uploader/configuration.rb
+++ lib/carrierwave/uploader/configuration.rb
@@ -109,7 +109,7 @@ module CarrierWave
           class_eval <<-RUBY, __FILE__, __LINE__ + 1
             def self.eager_load_fog(fog_credentials)
               # see #1198. This will hopefully no longer be necessary after fog 2.0
-              require self.fog_provider || 'fog'
+              require self.fog_provider if self.fog_provider.present?
               require 'carrierwave/storage/fog'
               Fog::Storage.new(fog_credentials) if fog_credentials.present?
             end
